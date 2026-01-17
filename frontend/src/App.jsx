399938b0f25cbeadb485fdbcecc8a20cb298ccfd
Vivek Singh <vviveksingh8874@gmail.com>
2026-01-17 17:20:53 +0530

    Added login page and axios logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from './pages/login';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-        
+      <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        </Routes>
+        </BrowserRouter>
     </>
   )
 }
